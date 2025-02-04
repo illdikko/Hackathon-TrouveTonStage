48cbc94284841f80eaad78c7c35804854f5dfbba
Meriton-AKJ <meritonaskaj01@gmail.com>
2025-02-04 14:54:32 +0100

    creation navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace utilisateur</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#"><img src="assets/048e0e3h.png" alt="logo_efp"></a>
+            <li><a href="#"></a>Catalogue</li>
+            <li><a href="#"></a>Contacts</li>
+            <li><a href="#"></a>Jubbibox</li>
+            <li><a href="#"></a>FAQ</li>
+            
+        </nav>
+    </header>
+    <h1>Espace utilisateur</h1>
+
+<section class="type_utilisateur">
+
+    <div class="category">
+        <h2><a href="https://student.efp.be/">Espace étudiant</a></h2>
+        <p>Tu es un étudiant à l'EFP ?</p>
+    </div>
+    <div class="formateur">
+        <h2><a href="https://formateur.efp.be/">Espace formateur</a></h2>
+        <p>Tu es un formateur à l'EFP ?</p>
+    </div>
+    
+    
+</section>
+   
+</body>
+</html>
