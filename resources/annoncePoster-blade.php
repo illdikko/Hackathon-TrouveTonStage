20e24fe417c79996779d3936edf0ecfad34dcb94
Meriton-AKJ <meritonaskaj01@gmail.com>
2025-02-04 16:03:47 +0100

    annonce poster n*10 créer

---
+++ resources/annoncePoster-blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annonce postée</title>
+</head>
+<body>
+    <h1>Les annonces postées</h1>
+    <h4>Annonce 1:</h4>
+    <h4>Annonce 2:</h4>
+    <h4>Annonce 3:</h4>
+    <h4>Annonce 4:</h4>
+    <h4>Annonce 5:</h4>
+    <h4>Annonce 6:</h4>
+    <h4>Annonce 7:</h4>
+    <h4>Annonce 8:</h4>
+    <h4>Annonce 9:</h4>
+    <h4>Annonce 10:</h4>
+    
+</body>
+</html>
