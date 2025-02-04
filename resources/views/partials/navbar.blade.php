133fd907d8148d6b608a44afc481dec627e3d9c9
Meriton-AKJ <meritonaskaj01@gmail.com>
2025-02-04 15:57:59 +0100

    rectification de navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,21 +17,5 @@
             
         </nav>
     </header>
-    <h1>Espace utilisateur</h1>
-
-<section class="type_utilisateur">
-
-    <div class="category">
-        <h2><a href="https://student.efp.be/">Espace étudiant</a></h2>
-        <p>Tu es un étudiant à l'EFP ?</p>
-    </div>
-    <div class="formateur">
-        <h2><a href="https://formateur.efp.be/">Espace formateur</a></h2>
-        <p>Tu es un formateur à l'EFP ?</p>
-    </div>
-    
-    
-</section>
-   
 </body>
 </html>
