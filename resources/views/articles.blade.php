74db3b06e1caa5841e7b3ea10d408a52b294dd6e
Rsinghefp <rabdeepshergill1@gmail.com>
2025-02-05 11:52:23 +0100

    main page

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Articles</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <nav>
+        <div class="logo"></div>
+        <div class="menu">
+            <div class="hamburger-menu"></div>
+            <button class="user-button">U</button>
+        </div>
+    </nav>
+
+    <div class="container">
+
+        <h2>Articles</h2>
+
+        <div class="article-list">
+            
+            <div class="article">
+                <img src="" alt="Image article">
+                <div class="text">
+                    <p><strong>Titre de l'article</strong></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="article">
+                <img src="" alt="Image article">
+                <div class="text">
+                    <p><strong>Titre de l'article</strong></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="article">
+                <img src="" alt="Image article">
+                <div class="text">
+                    <p><strong>Titre de l'article</strong></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/chercheStage.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Je cherche un stage</title>
+</head>
+<body>
+    <nav>
+        <div class="logo"></div>
+        <div class="menu">
+            <div class="hamburger-menu"></div>
+            <button class="user-button">U</button>
+        </div>
+    </nav>
+
+
+    <main>
+        <h2>Je cherche un stage.</h2>
+        <div class="grid-container">
+            <div class="stage">
+                <img src="" alt="">
+                <p>Référent</p>
+            </div>
+            <div class="stage">
+                <img src="" alt="">
+                <p>Référent</p>
+            </div>
+            <div class="stage">
+                <img src="" alt="">
+                <p>Référent</p>
+            </div>
+            <div class="stage">
+                <img src="" alt="">
+                <p>Référent</p>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
