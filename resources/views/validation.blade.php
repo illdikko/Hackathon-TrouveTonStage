7d0f5c444097370ad06f6bd68c3e680eda476242
spobb <gverlaeken@gmail.com>
2025-02-06 12:34:54 +0100

    page validation

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -1,7 +1,9 @@
 @include('partials.header')
 
-<img src="./public/yellowCheckmark.png"></img>
-<h1 class="cyan-writing" style="font-size: 14">Merci!</h1>
-<h3 class="blue-writing" style="font-size: 11">Votre demande nous a bien été transmise!</h3>
+<div class="d-flex flex-column align-items-center m-5">
+    <img class="mb-5" src="{{ asset('/yellowCheckmark.png') }}" height="256" width="256"></img>
+    <h1 class="cyan-writing" style="font-size: 14">Merci!</h1>
+    <h3 class="blue-writing" style="font-size: 11">Votre demande nous a bien été transmise!</h3>
+</div>
 
 @include('partials.footer')
