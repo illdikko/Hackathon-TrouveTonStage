13ef5587e63b50054177a2d029c672ec64421d1b
Coline <coline@efarmz.be>
2025-02-06 10:55:13 +0100

    corrected CSS

--- resources/views/jobibox.blade.php
+++ resources/views/jobibox.blade.php
@@ -7,7 +7,7 @@
     Vidéo de présentation de jobibox
 </video>
 
-<h2>Qu'est ce que la jobibox ?</h2>
+<h2 class="section-title">Qu'est ce que la jobibox ?</h2>
 <p>La Jobibox est une plateforme en ligne qui facilite la recherche de stages, d’emplois et d’alternances. <br>Elle met
     en relation les entreprises et les candidats via une interface intuitive et accessible.</p>
 
